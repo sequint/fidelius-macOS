ff5417a565f924ca7d43a446d073443db1089c36
stevenquintana <steve.e.quint@gmail.com>
2024-05-06 13:59:08 -0500

    Initial fidelius macos commit

---
+++ fidelius-macOS/Views/Title.swift
@@ -0,0 +1,18 @@
+//
+//  Title.swift
+//  fidelius-macOS
+//
+//  Created by Steven Quintana on 5/6/24.
+//
+
+import SwiftUI
+
+struct Title: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Title()
+}
