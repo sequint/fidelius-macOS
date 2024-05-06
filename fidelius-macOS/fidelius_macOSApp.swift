5f91bd10b59073d85a55c1c007e6fafd28581a0c
stevenquintana <steve.e.quint@gmail.com>
2024-05-06 13:53:42 -0500

    Initial Commit

---
+++ fidelius-macOS/fidelius_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  fidelius_macOSApp.swift
+//  fidelius-macOS
+//
+//  Created by Steven Quintana on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct fidelius_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
